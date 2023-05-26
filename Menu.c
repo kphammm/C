9464d75c3af8b2aa37ff48401eda810047b1a7a6
kphammm <122054364+kphammm@users.noreply.github.com>
2023-05-25 21:44:13 -0700

    Update Menu.c

--- Menu.c
+++ Menu.c
@@ -5,6 +5,24 @@
 *Programming Assignment #2
 */
 
+/*
+This is Menu Driven program that can be run using
+user@user-VirtualBox:~$ gcc file2.c
+user@user-VirtualBox:~$ ./a.out
+
+Once you run the program, it will ask for an option. You must enter a numerical value for the program to run. Enter a number 1-5 to go through 
+
+1. Name (Enter a String value that is less than 100 characters)
+2. Age (Enter an Int value)
+3. Points (Enter a Float Value)
+4. Display (Will display the data and indicate which value are unknown)
+5. Clear  (Will clear the data)
+
+Option 6 will exit the program
+If you enter a number that is not 1-6 then the program will repeat the menu again and give directions to follow
+If you enter a any value that is not numerical, the program will crash
+*/
+
 #include <stdio.h>
 
 /*Method: main
